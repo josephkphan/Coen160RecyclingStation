13d610aa909fc9e655c68c43466c167ffc094533
thomasvn <thomasnguyen96@gmail.com>
2017-02-19 15:25:23 -0800

    Finished `addRecyclableItem` method. Need to implement method to log transaction and return Money

--- src/machine/RecyclingMachine.java
+++ src/machine/RecyclingMachine.java
@@ -2,17 +2,17 @@ package machine;
 
 import recyclable.RecyclableItem;
 import recyclable.Glass;
+import recyclable.Metal;
 import recyclable.Paper;
 import recyclable.Plastic;
-//import recyclable.Metal; // TODO: Make Metal Recyclable Items
 
 
 public class RecyclingMachine {
     // Capacity Related Data Members
-    private double maxPaperLoad, currentPaperLoad;
     private double maxGlassLoad, currentGlassLoad;
-    private double maxPlasticLoad, currentPlasticLoad;
     private double maxMetalLoad, currentMetalLoad;
+    private double maxPaperLoad, currentPaperLoad;
+    private double maxPlasticLoad, currentPlasticLoad;
 
     // Machine Information
     private int id;
@@ -20,88 +20,168 @@ public class RecyclingMachine {
     private double money, availableMoney;
 
     // Setters and Getters
-    public double getMaxPaperLoad() { return maxPaperLoad; }
+    public double getMaxPaperLoad() {
+        return maxPaperLoad;
+    }
 
-    public void setMaxPaperLoad(double maxPaperLoad) { this.maxPaperLoad = maxPaperLoad; }
+    public double getCurrentPaperLoad() {
+        return currentPaperLoad;
+    }
 
-    public double getCurrentPaperLoad() { return currentPaperLoad; }
+    public void setCurrentPaperLoad(double currentPaperLoad) {
+        this.currentPaperLoad = currentPaperLoad;
+    }
 
-    public void setCurrentPaperLoad(double currentPaperLoad) { this.currentPaperLoad = currentPaperLoad; }
+    public double getMaxGlassLoad() {
+        return maxGlassLoad;
+    }
 
-    public double getMaxGlassLoad() { return maxGlassLoad; }
+    public double getCurrentGlassLoad() {
+        return currentGlassLoad;
+    }
 
-    public void setMaxGlassLoad(double maxGlassLoad) { this.maxGlassLoad = maxGlassLoad; }
+    public void setCurrentGlassLoad(double currentGlassLoad) {
+        this.currentGlassLoad = currentGlassLoad;
+    }
 
-    public double getCurrentGlassLoad() { return currentGlassLoad; }
+    public double getMaxPlasticLoad() {
+        return maxPlasticLoad;
+    }
 
-    public void setCurrentGlassLoad(double currentGlassLoad) { this.currentGlassLoad = currentGlassLoad; }
+    public double getCurrentPlasticLoad() {
+        return currentPlasticLoad;
+    }
 
-    public double getMaxPlasticLoad() { return maxPlasticLoad; }
+    public void setCurrentPlasticLoad(double currentPlasticLoad) {
+        this.currentPlasticLoad = currentPlasticLoad;
+    }
 
-    public void setMaxPlasticLoad(double maxPlasticLoad) { this.maxPlasticLoad = maxPlasticLoad; }
+    public double getMaxMetalLoad() {
+        return maxMetalLoad;
+    }
 
-    public double getCurrentPlasticLoad() { return currentPlasticLoad; }
+    public double getCurrentMetalLoad() {
+        return currentMetalLoad;
+    }
 
-    public void setCurrentPlasticLoad(double currentPlasticLoad) { this.currentPlasticLoad = currentPlasticLoad; }
+    public void setCurrentMetalLoad(double currentMetalLoad) {
+        this.currentMetalLoad = currentMetalLoad;
+    }
 
-    public double getMaxMetalLoad() { return maxMetalLoad; }
+    public int getId() {
+        return id;
+    }
 
-    public void setMaxMetalLoad(double maxMetalLoad) { this.maxMetalLoad = maxMetalLoad; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public double getCurrentMetalLoad() { return currentMetalLoad; }
+    public int getxCoord() {
+        return xCoord;
+    }
 
-    public void setCurrentMetalLoad(double currentMetalLoad) { this.currentMetalLoad = currentMetalLoad; }
+    public void setxCoord(int xCoord) {
+        this.xCoord = xCoord;
+    }
 
-    public int getId() { return id; }
+    public int getyCoord() {
+        return yCoord;
+    }
 
-    public void setId(int id) { this.id = id; }
+    public void setyCoord(int yCoord) {
+        this.yCoord = yCoord;
+    }
 
-    public int getxCoord() { return xCoord; }
+    public double getMoney() {
+        return money;
+    }
 
-    public void setxCoord(int xCoord) { this.xCoord = xCoord; }
+    public void setMoney(double money) {
+        this.money = money;
+    }
 
-    public int getyCoord() { return yCoord; }
+    public double getAvailableMoney() {
+        return availableMoney;
+    }
 
-    public void setyCoord(int yCoord) { this.yCoord = yCoord; }
-
-    public double getMoney() { return money; }
-
-    public void setMoney(double money) { this.money = money; }
-
-    public double getAvailableMoney() { return availableMoney; }
-
-    public void setAvailableMoney(double availableMoney) { this.availableMoney = availableMoney; }
+    public void setAvailableMoney(double availableMoney) {
+        this.availableMoney = availableMoney;
+    }
 
     // Constructor
     public RecyclingMachine(int xCoord, int yCoord, int id) {
-        this.maxPaperLoad= 100;
         this.maxGlassLoad = 100;
-        this.maxPlasticLoad = 100;
         this.maxMetalLoad = 100;
+        this.maxPaperLoad = 100;
+        this.maxPlasticLoad = 100;
 
-        this.currentPaperLoad = 0;
         this.currentGlassLoad = 0;
-        this.currentPlasticLoad = 0;
         this.currentMetalLoad = 0;
+        this.currentPaperLoad = 0;
+        this.currentPlasticLoad = 0;
 
         this.xCoord = xCoord;
         this.yCoord = yCoord;
         this.id = id;
     }
 
+    public RecyclingMachine(int xCoord, int yCoord, int id, double maxGlassLoad, double maxMetalLoad, double maxPaperLoad, double maxPlasticLoad) {
+        this.maxGlassLoad = maxGlassLoad;
+        this.maxMetalLoad = maxMetalLoad;
+        this.maxPaperLoad = maxPaperLoad;
+        this.maxPlasticLoad = maxPlasticLoad;
+
+        this.currentGlassLoad = 0;
+        this.currentMetalLoad = 0;
+        this.currentPaperLoad = 0;
+        this.currentPlasticLoad = 0;
+
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.id = id;
+    }
+
+
     // Start
     // TODO: Create different states for the recycling machine
 
-    // Insert Item
+    // Insert Recyclable Item
     public boolean addRecyclableItem(RecyclableItem r) {
         // Check whether the item will fit inside the load
+        if (r instanceof Glass) {
+            if (r.getWeight() + getCurrentGlassLoad() > getMaxGlassLoad()) {
+                return false;
+            }
+            setCurrentGlassLoad(getCurrentGlassLoad() + r.getWeight());
 
-        // Add item to the current load
+        } else if (r instanceof Metal) {
+            if (r.getWeight() + getCurrentMetalLoad() > getMaxMetalLoad()) {
+                return false;
+            }
+            setCurrentMetalLoad(getCurrentMetalLoad() + r.getWeight());
 
-        // Call Function to Log a Transaction
+        } else if (r instanceof Paper) {
+            if (r.getWeight() + getCurrentPaperLoad() > getMaxPaperLoad()) {
+                return false;
+            }
+            setCurrentPaperLoad(getCurrentPaperLoad() + r.getWeight());
 
-        // Call Return Money Function
+        } else if (r instanceof Plastic) {
+            if (r.getWeight() + getCurrentPlasticLoad() > getMaxPlasticLoad()) {
+                return false;
+            }
+            setCurrentPlasticLoad(getCurrentPlasticLoad() + r.getWeight());
 
+        } else {
+            System.out.println("You didn't insert a recyclable item. You are a terrible person.");
+            return false;
+        }
+
+        // TODO: For each case, you still need to log the transaction and return money
+
+        // Log Transaction Function
+
+        // Return Money Function
 
         return true;
     }
